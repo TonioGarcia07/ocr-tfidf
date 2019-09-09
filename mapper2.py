12489e2e51ca877676587e8e0d1e8232a87e97db
AntoineG <antoine.garcia@polyconseil.fr>
2019-09-09 23:56:44 +0200

    clean comments

--- mapper2.py
+++ mapper2.py
@@ -2,26 +2,6 @@
 
 import sys
 
-# Input:
-# yelling,1       2
-# yellings,1      1
-# yellow,1        1
-# yellow,2        5
-
-# Map Output:
-# 1         yelling,2
-# 1         yellings,1
-# 1         yellow,1
-# 2         yellow,5
-
-# Shuffle-sort output:
-# (1, [(yelling,2), (yellings,1), (yellow,1)]
-# (2, [(yellow,5)]
-
-# Reduce output:
-# (1, [(yelling,2), (yellings,1), (yellow,1)]
-# (2, [(yellow,5)]
-
 for line in sys.stdin:
     line = line.strip()
 
